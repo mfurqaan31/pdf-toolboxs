7a22ce6635cf4fa4706a934f783ba2db2e7556ef
Mohammed Furqaan <furqaanpatel2004@gmail.com>
2024-02-04 00:44:27 +0530

    Update split.py

--- split.py
+++ split.py
@@ -31,7 +31,7 @@ def split_pdf(pdf_path, page_ranges):
         start = max(1, start)  # Ensure start is not less than 1
         end = min(total_pages, end)  # Ensure end is not greater than total pages
 
-        pdf_writer = PyPDF2.PdfWriter()  # Use PdfWriter instead of PdfFileWriter
+        pdf_writer = PyPDF2.PdfWriter()
         for page_num in range(start - 1, end):
             pdf_writer.add_page(pdf_reader.pages[page_num])
 
@@ -42,13 +42,23 @@ def split_pdf(pdf_path, page_ranges):
     pdf_file.close()
     return split_pdfs
 
+def merge_pdfs(split_pdfs):
+    merged_pdf_writer = PyPDF2.PdfWriter()
+    for pdf_bytes in split_pdfs:
+        pdf_reader = PyPDF2.PdfReader(pdf_bytes)
+        for page_num in range(len(pdf_reader.pages)):
+            merged_pdf_writer.add_page(pdf_reader.pages[page_num])
+
+    merged_pdf_bytes = BytesIO()
+    merged_pdf_writer.write(merged_pdf_bytes)
+    return merged_pdf_bytes
+
 def main():
     st.title("PDF Page Splitter")
 
     uploaded_file = st.file_uploader("Upload a PDF file", type=["pdf"])
 
     if uploaded_file is not None:
-        # Save the uploaded file to the 'uploads' folder
         pdf_path = os.path.join("uploads", uploaded_file.name)
 
         os.makedirs("uploads", exist_ok=True)
@@ -57,30 +67,34 @@ def main():
 
         check_encrypted(pdf_path)
 
-        # Initialize total_pages here
         pdf_reader = PyPDF2.PdfReader(pdf_path)
         total_pages = len(pdf_reader.pages)
 
         page_ranges = st.text_input("Enter page ranges (e.g., 1-3, 4-6):")
-        zip_filename = st.text_input("Enter the desired name for the zip file (without extension):")
+        zip_filename = st.text_input("Enter the name for the output file without any extension:")
+        zip_filename=zip_filename.split('.')[0]
+        merge_checkbox = st.checkbox("Merge all ranges")
 
-        if page_ranges.strip() and zip_filename.strip():  # Check if the inputs are not empty
+        if page_ranges.strip() and zip_filename.strip():
             try:
                 ranges = [list(map(int, rng.split('-'))) for rng in page_ranges.split(',')]
                 if all(1 <= start <= end <= total_pages for start, end in ranges):
                     if st.button("Split PDF"):
                         split_pdfs = split_pdf(pdf_path, ranges)
 
-                        # Create a zip file with the user-specified name in the 'uploads' directory
-                        zip_filename_with_extension = os.path.join("uploads", f"{zip_filename}.zip")
-                        with zipfile.ZipFile(zip_filename_with_extension, 'w') as zip_file:
-                            for i, pdf_bytes in enumerate(split_pdfs):
-                                pdf_filename = f"split_pdf_{i + 1}.pdf"
-                                zip_file.writestr(pdf_filename, pdf_bytes.getvalue())
+                        if merge_checkbox:
+                            merged_pdf_bytes = merge_pdfs(split_pdfs)
+                            st.success(f"The PDFs have been merged, and the PDF is ready for Download.")
+                            st.download_button(label=f"Download {zip_filename}.pdf", data=merged_pdf_bytes.getvalue(), file_name=f"{zip_filename}.pdf")
+                        else:
+                            zip_filename_with_extension = os.path.join("uploads", f"{zip_filename}.zip")
+                            with zipfile.ZipFile(zip_filename_with_extension, 'w') as zip_file:
+                                for i, pdf_bytes in enumerate(split_pdfs):
+                                    pdf_filename = f"split_pdf_{i + 1}.pdf"
+                                    zip_file.writestr(pdf_filename, pdf_bytes.getvalue())
 
-                        # Provide a single download button for the zip file
-                        st.success(f"The PDFs have been split, and the ZIP file is ready for Download.")
-                        st.download_button(label=f"Download {zip_filename}.zip", data=open(zip_filename_with_extension, 'rb').read(), file_name=f"{zip_filename}.zip")
+                            st.success(f"The PDFs have been split, and the ZIP file is ready for Download.")
+                            st.download_button(label=f"Download {zip_filename}.zip", data=open(zip_filename_with_extension, 'rb').read(), file_name=f"{zip_filename}.zip")
 
                 else:
                     st.warning("Invalid page range. Make sure all ranges are in the correct format and within the total number of pages.")
