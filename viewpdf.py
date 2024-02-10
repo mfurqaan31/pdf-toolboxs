012d224c052df391d1df57cdee5c12ab5cafde0a
Mohammed Furqaan <furqaanpatel2004@gmail.com>
2024-02-11 01:47:29 +0530

    main viewpdf.py

--- viewpdf.py
+++ viewpdf.py
@@ -1,35 +1,56 @@
-# has slider
+# main code
+import os
 import streamlit as st
-import io
-from PIL import Image
 from pdf2image import convert_from_bytes
-import fitz  # PyMuPDF
+import fitz
+import PyPDF2
+import atexit
+import shutil
 
-def display_pdf_page(pdf_bytes, page_number):
-    pdf_document = fitz.open(stream=io.BytesIO(pdf_bytes), filetype="pdf")
+def cleanup():
+    shutil.rmtree("uploads", ignore_errors=True)
+
+def check_encrypted(pdf_file_path):
+    is_encrypted = False
+    with open(pdf_file_path, 'rb') as pdf_file:
+        pdf_reader = PyPDF2.PdfReader(pdf_file)
+        is_encrypted = pdf_reader.is_encrypted
+
+    if is_encrypted:
+        st.error("The selected PDF is encrypted.")
+        st.stop()
+
+def display_pdf_page(uploaded_file, page_number):
+    pdf_document = fitz.open(stream=uploaded_file.getvalue(), filetype="pdf")
     if page_number <= 0 or page_number > pdf_document.page_count:
         st.write(f"Invalid page number. Please enter a page number between 1 and {pdf_document.page_count}.")
     else:
-        # Convert the PDF page to an image
+        pdf_bytes = uploaded_file.read()
         images = convert_from_bytes(pdf_bytes, first_page=page_number, last_page=page_number)
         if images:
-            # Display the image
             st.image(images[0], caption=f"Page {page_number}", use_column_width=True)
         else:
             st.write("Error converting PDF to image.")
 
 def main():
-    st.title("Edit PDF")
-    # Upload PDF file
+    st.title("View your PDF")
+    if not os.path.exists("uploads"):
+        os.makedirs("uploads")
+
     uploaded_file = st.file_uploader("Upload PDF", type=["pdf"])
 
     if uploaded_file is not None:
-        pdf_bytes = uploaded_file.read()
-        page_number = st.slider("Select page number", 1, fitz.open(stream=io.BytesIO(pdf_bytes), filetype="pdf").page_count, 1)
-        display_pdf_page(pdf_bytes, page_number)
+        file_path = os.path.join("uploads", uploaded_file.name)
+        with open(file_path, "wb") as f:
+            f.write(uploaded_file.getvalue())
 
+        check_encrypted(file_path)
+
+        page_number = st.number_input("Enter page number", min_value=1, value=1)
+        display_pdf_page(uploaded_file, page_number)
     else:
         st.write("Please upload a PDF file.")
 
 if __name__ == "__main__":
+    atexit.register(cleanup)
     main()
