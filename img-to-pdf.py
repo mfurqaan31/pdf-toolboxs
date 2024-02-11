6dc632f8f5c0442a884f49715bda707ed258bcb7
Mohammed Furqaan <furqaanpatel2004@gmail.com>
2024-02-11 16:52:20 +0530

    main img-to-pdf.py

--- img-to-pdf.py
+++ img-to-pdf.py
@@ -1,45 +1,38 @@
+# main code
 import streamlit as st
 from streamlit_sortables import sort_items
 from PIL import Image
-import io
-import os
-import shutil
+import os, shutil
 import atexit
 from reportlab.pdfgen import canvas
 
-def remove_uploads_folder():
-    # Remove the "uploads" folder when the program exits
+def cleanup():
     shutil.rmtree("uploads", ignore_errors=True)
 
 def save_uploaded_images(uploaded_images):
     uploaded_paths = []
     uploaded_image_names = set()
 
-    # Create the 'uploads' directory if it doesn't exist
     if not os.path.exists("uploads"):
         os.makedirs("uploads")
 
     for i, uploaded_image in enumerate(uploaded_images, start=1):
-        # Get the original filename and change the extension to .png
         original_filename, ext = os.path.splitext(uploaded_image.name)
         new_filename = f"{original_filename}.png"
-
-        # Check for duplicates and append a suffix if necessary
         counter = 1
+
         while new_filename in uploaded_image_names:
             new_filename = f"{original_filename}({counter}).png"
             counter += 1
 
         uploaded_image_names.add(new_filename)
-
-        # Save the image with the new filename and .png extension
         image_path = os.path.join("uploads", new_filename)
+
         with open(image_path, "wb") as f:
             f.write(uploaded_image.read())
         uploaded_paths.append(image_path)
 
     return uploaded_paths
-
 
 def display_images(uploaded_paths, reordered_paths):
     images_per_row = 3
@@ -48,10 +41,8 @@ def display_images(uploaded_paths, reordered_paths):
     
     for i in range(num_rows):
         row_images = reordered_paths[i * images_per_row: (i + 1) * images_per_row]
+        st.write("\n")
 
-        st.write("\n")  # Add a newline for spacing
-
-        # Display three images horizontally in a row
         row_images_display = []
         for filename in row_images:
             index = uploaded_paths.index(os.path.join("uploads", filename))
@@ -59,16 +50,14 @@ def display_images(uploaded_paths, reordered_paths):
 
             try:
                 image = Image.open(image_path)
-                image = image.resize((200, 200))  # Resize the image to 200x200
+                image = image.resize((200, 200))
                 row_images_display.append(image)
             
             except OSError:
                 pass
 
-        # Display the images with three images per row
         st.image(row_images_display, caption=[f"Page {i * images_per_row + j + 1}: {os.path.basename(filename)}" for j, filename in enumerate(row_images)], width=200, use_column_width=False)
-        st.write("\n")  # Add a newline for spacing
-        
+        st.write("\n")
 
 def convert_images_to_pdf(image_paths, output_pdf_path, page_size=(800, 600)):
     pdf_canvas = canvas.Canvas(output_pdf_path)
@@ -85,25 +74,17 @@ def convert_images_to_pdf(image_paths, output_pdf_path, page_size=(800, 600)):
 
 def main():
     st.title("Images to PDF")
-
     uploaded_images = st.file_uploader("Upload Images", type=["jpg", "jpeg", "png"], accept_multiple_files=True)
 
     if uploaded_images:
-        # Save uploaded images to the "uploads" folder
         uploaded_paths = save_uploaded_images(uploaded_images)
-
-        # Display the images for rearranging
         st.write("Arrange the order of the images")
-        reordered_paths = sort_items([os.path.basename(path) for path in uploaded_paths])  # Use sort_items with filenames
+        reordered_paths = sort_items([os.path.basename(path) for path in uploaded_paths])
         st.write("Order for the uploaded images:")
-
-        # Display the images with three images per row
         display_images(uploaded_paths, reordered_paths)
-
         output_pdf_name = st.text_input("Enter the name of the output PDF file (without extension):", "merged_images_pdf")
 
         if st.button("Convert to PDF"):
-            # Append ".pdf" extension if not provided by the user
             output_pdf_path = os.path.join("uploads", f"{output_pdf_name}.pdf" if not output_pdf_name.endswith(".pdf") else output_pdf_name)
             convert_images_to_pdf([os.path.join("uploads", filename) for filename in reordered_paths], output_pdf_path)
 
@@ -117,5 +98,5 @@ def main():
             )
 
 if __name__ == "__main__":
-    atexit.register(remove_uploads_folder)
+    atexit.register(cleanup)
     main()
