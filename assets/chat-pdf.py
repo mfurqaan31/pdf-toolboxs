fac674476f139e88c2f8762759676edfe7909f12
Mohammed Furqaan <furqaanpatel2004@gmail.com>
2024-02-12 22:59:59 +0530

    Update chat-pdf.py

--- assets/chat-pdf.py
+++ assets/chat-pdf.py
@@ -22,9 +22,10 @@ def cleanup():
 def get_pdf_text(pdf_docs):
     text = ""
     for pdf in pdf_docs:
-        pdf_reader = PdfReader(pdf)
-        for page in pdf_reader.pages:
-            text += page.extract_text()
+        with open(pdf, 'rb') as f:
+            pdf_reader = PdfReader(f)
+            for page in pdf_reader.pages:
+                text += page.extract_text()
     return text
 
 def get_text_chunks(text):
