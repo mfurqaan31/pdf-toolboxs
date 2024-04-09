6fa5756f8151b2390e550b8ecf51b054053579a0
Mohammed Furqaan <furqaanpatel2004@gmail.com>
2024-04-09 22:06:16 +0530

    Update view-pdf.py

--- assets/view-pdf.py
+++ assets/view-pdf.py
@@ -1,11 +1,10 @@
 # main code
 import os
 import streamlit as st
-from pdf2image import convert_from_bytes
-import fitz
 import PyPDF2
 import atexit
 import shutil
+import base64
 
 def cleanup():
     shutil.rmtree("uploads", ignore_errors=True)
@@ -20,20 +19,19 @@ def check_encrypted(pdf_file_path):
         st.error("The selected PDF is encrypted.")
         st.stop()
 
-def display_pdf_page(uploaded_file, page_number):
-    pdf_document = fitz.open(stream=uploaded_file.getvalue(), filetype="pdf")
-    if page_number <= 0 or page_number > pdf_document.page_count:
-        st.write(f"Invalid page number. Please enter a page number between 1 and {pdf_document.page_count}.")
-    else:
-        pdf_bytes = uploaded_file.read()
-        images = convert_from_bytes(pdf_bytes, first_page=page_number, last_page=page_number)
-        if images:
-            st.image(images[0], caption=f"Page {page_number}", use_column_width=True)
-        else:
-            st.write("Error converting PDF to image.")
+def displayPDF(file):
+    # Opening file from file path
+    with open(file, "rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+
+    # Embedding PDF in HTML
+    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="800" type="application/pdf"></iframe>'
+
+    # Displaying File
+    st.markdown(pdf_display, unsafe_allow_html=True)
 
 def main():
-    st.title("PDF Viewer 🔎")
+    st.title("View your PDF")
     if not os.path.exists("uploads"):
         os.makedirs("uploads")
 
@@ -45,9 +43,8 @@ def main():
             f.write(uploaded_file.getvalue())
 
         check_encrypted(file_path)
-
-        page_number = st.number_input("Enter page number", min_value=1, value=1)
-        display_pdf_page(uploaded_file, page_number)
+        
+        displayPDF(file_path)
     else:
         st.write("Please upload a PDF file.")
 
